2d68ebdd85b89d8a51d5fd7a1b9beb8d4f76af58
lui2mi <lui2mi@lui2mi-desktop>
2014-08-12 22:36:15 -0500

    7 agregado splas screen cambiado contenido solo potrait

--- INN-EMP/gen/com/uac/inn_emp/R.java
+++ INN-EMP/gen/com/uac/inn_emp/R.java
@@ -19,8 +19,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int escudo1=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
         public static final int Contenido=0x7f070004;
@@ -36,6 +37,7 @@ public final class R {
         public static final int listado=0x7f030002;
         public static final int lstcontenido=0x7f030003;
         public static final int lsttitulo=0x7f030004;
+        public static final int ss=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
